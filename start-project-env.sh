a48f118d87e5424d1d62d2e8b7819a272794757c
ammarrqureshi <mamqureshi786@gmail.com>
2024-08-25 12:45:08 +0500

    shell script updated

--- start-project-env.sh
+++ start-project-env.sh
@@ -15,11 +15,21 @@ if [ ! -d "$project_dir" ]; then
   exit 1
 fi
 
+# Automatically generate a unique session name by incrementing a number
+base_name="myproject"
+session_num=1
+
+while tmux has-session -t ${base_name}${session_num} 2>/dev/null; do
+  session_num=$((session_num + 1))
+done
+
+session_name="${base_name}${session_num}"
+
 # Change to the projects directory
 cd "$project_dir" || exit
 
-# Start a new tmux session named "myproject"
-tmux new-session -d -s myproject
+# Start a new tmux session with the unique session name
+tmux new-session -d -s "$session_name"
 
 # Split the window horizontally
 tmux split-window -h
@@ -28,7 +38,7 @@ tmux split-window -h
 tmux new-window -n editor
 
 # Start Neovim in the new window
-tmux send-keys -t myproject:editor 'nvim' C-m
+tmux send-keys -t "$session_name:editor" 'nvim' C-m
 
 # Attach to the tmux session
-tmux attach-session -t myproject
+tmux attach-session -t "$session_name"
